cd4408297bcc76ff9c19b6a3816360458d13bb5c
IanChen <ian806.chen@gmail.com>
2024-01-24 11:10:58 +0800

    天數最少的上色

---
+++ DungBeetle/Components/Pages/DaySummaryComponent/MaxDaySummary.cs
@@ -0,0 +1,16 @@
+﻿namespace DungBeetle.Components.Pages.DaySummaryComponent;
+
+public class MaxDaySummary
+{
+    public int FirstHoliday { get; set; }
+
+    public int FirstWorkDay { get; set; }
+
+    public int SecondHoliday { get; set; }
+
+    public int SecondWorkDay { get; set; }
+
+    public int FirstDay { get; set; }
+
+    public int SecondDay { get; set; }
+}
---
+++ DungBeetle/Components/Pages/DaySummaryComponent/MemberDaySummary.cs
@@ -0,0 +1,20 @@
+﻿namespace DungBeetle.Components.Pages.DaySummaryComponent;
+
+public class MemberDaySummary
+{
+    public string Name { get; set; } = "";
+
+    public int FirstHolidayDays { get; set; }
+
+    public int FirstWorkDays { get; set; }
+
+    public int FirstDays { get; set; }
+
+    public int SecondDays { get; set; }
+
+    public int SecondHolidayDays { get; set; }
+
+    public int SecondWorkDays { get; set; }
+
+    public int Total => FirstDays + SecondDays;
+}
